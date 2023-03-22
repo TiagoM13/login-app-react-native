e0e971e71446ae7f61b3d8effae4f4d71092f216
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-22 16:24:10 -0300

    create custom authentication hook

---
+++ src/hooks/auth.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
+
+export const useAuthentication = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // get email field value
+  const onEmail = (value: string) => {
+    setEmail(value);
+  };
+
+  // get password field value
+  const onPassword = (value: string) => {
+    setPassword(value);
+  };
+
+  const handleAuthentication = () => {
+    // validate if fields are empty if else login user
+    if (email.trim() === '' && password.trim() === '') {
+      Alert.alert('Invalid fields!', 'Fields must be filled');
+    } else {
+      Alert.alert('Authentication', 'User successfully authenticated!');
+      console.warn({ email, password });
+    }
+
+    // reset fields
+    setEmail('');
+    setPassword('');
+  };
+
+  return {
+    email,
+    password,
+    onEmail,
+    onPassword,
+    handleAuthentication,
+  };
+};
