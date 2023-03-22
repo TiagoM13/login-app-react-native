7eea6c06696e892c49da546486c91658aed42c0f
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-22 16:41:18 -0300

    validate email with regex

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -5,6 +5,9 @@ export const useAuthentication = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  // regex validate email
+  const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
   // get email field value
   const onEmail = (value: string) => {
     setEmail(value);
@@ -17,8 +20,10 @@ export const useAuthentication = () => {
 
   const handleAuthentication = () => {
     // validate if fields are empty if else login user
-    if (email.trim() === '' && password.trim() === '') {
+    if (email.trim() === '' || password.trim() === '') {
       Alert.alert('Invalid fields!', 'Fields must be filled');
+    } else if (!REGEX_EMAIL.test(email)) {
+      Alert.alert('Invalid email!', 'Email invalid');
     } else {
       Alert.alert('Authentication', 'User successfully authenticated!');
       console.warn({ email, password });
