773e5292640bbaba2199b033e4dbb8f433143c4f
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-22 16:24:42 -0300

    use custom authentication hook

--- src/screens/SIgnIn.tsx
+++ src/screens/SIgnIn.tsx
@@ -1,31 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { TouchableButton } from '../components/Button/TouchableButton';
 import { InputFiled } from '../components/InputFiled/InputField';
+import { useAuthentication } from '../hooks/auth';
 
 import { styles } from './styles';
 
 const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = () => {
-    // validate email and password
-    // send login request to server
-    // navigate to home screen if successful
-
-    console.log({ email, password });
-
-    setEmail('');
-    setPassword('');
-  };
+  const { email, password, onEmail, onPassword, handleAuthentication } = useAuthentication();
 
   return (
     <View style={styles.container}>
       <InputFiled
         label="Email"
         value={email}
-        onChangeText={(text) => setEmail(text)}
+        onChangeText={text => onEmail(text)}
         placeholder="Enter your email"
         keyboardType="email-address"
       />
@@ -33,12 +22,12 @@ const SignIn = () => {
       <InputFiled
         label="Password"
         value={password}
-        onChangeText={(text) => setPassword(text)}
+        onChangeText={text => onPassword(text)}
         placeholder="Enter your password"
         secureTextEntry={true}
       />
 
-      <TouchableButton value="Sign In" onPress={handleLogin} />
+      <TouchableButton value="Sign In" onPress={handleAuthentication} />
     </View>
   );
 };
