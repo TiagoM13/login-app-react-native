43efa949c055dd032ad179bb70e72db80f48daff
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-23 12:28:42 -0300

    add components on signup screen

--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { TouchableButton } from '../../components/Button/TouchableButton';
+import { InputFiled } from '../../components/InputFiled/InputField';
 
 import { styles } from './styles';
 
 const SignUp = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>Sign Up</Text>
+
+      <InputFiled label="Name" keyboardType="default" placeholder="Enter your name" />
+
+      <InputFiled label="Email" placeholder="Enter your email" keyboardType="email-address" />
+
+      <InputFiled label="Password" placeholder="Enter your password" secureTextEntry={true} />
+
+      <TouchableButton value="Sign Up" onPress={() => console.warn('Enviado com sucesso!')} />
     </View>
   );
 };
