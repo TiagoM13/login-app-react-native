d3907d7aca999ebf2d695c01ac4e7f20df52944c
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-23 12:03:52 -0300

    create sign up screen

---
+++ src/screens/SignUp/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text style={styles.title}>Sign Up</Text>
+    </View>
+  );
+};
+
+export default SignUp;
