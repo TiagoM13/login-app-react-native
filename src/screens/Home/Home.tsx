391cd9e01d1cabaccc162e67eac2c2c2d3b7f966
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-26 12:12:42 -0300

    navigate between routes

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Image, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import img from '../../assets/img/user.png';
 
+import { ScreensNavigationProps } from '../../@types/navigation';
 import { TouchableButton } from '../../components/Button/TouchableButton';
 import { THEME } from '../../utils/theme';
 
 import { styles } from './styles';
 
 const Home = () => {
+  const navigation = useNavigation<ScreensNavigationProps>();
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={img} />
@@ -18,8 +22,12 @@ const Home = () => {
         <Text style={styles.subtitle}>You need to create an account or login to log in to the app</Text>
       </View>
 
-      <TouchableButton value="Login" />
-      <TouchableButton value="Sign Up" background={THEME.RED_500} />
+      <TouchableButton value="Login" onPress={() => navigation.navigate('SignIn')} />
+      <TouchableButton
+        value="Sign Up"
+        background={THEME.RED_500}
+        onPress={() => navigation.navigate('SignUp')}
+      />
     </View>
   );
 };
