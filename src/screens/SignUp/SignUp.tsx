daf8fb09aefed80f3e3c0882d0fc6638dfef65fa
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-28 12:39:19 -0300

    use custom hook on signup screen

--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -2,21 +2,42 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { TouchableButton } from '../../components/Button/TouchableButton';
 import { InputFiled } from '../../components/InputFiled/InputField';
+import { useCreateUser } from '../../hooks/create';
 
 import { styles } from '../styles/styles';
 
 const SignUp = () => {
+  const { user, email, password, getUser, getEmail, getPassword, handleCreateAccount } = useCreateUser();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sign Up</Text>
 
-      <InputFiled label="Name" keyboardType="default" placeholder="Enter your name" />
+      <InputFiled
+        label="Name"
+        value={user}
+        onChangeText={text => getUser(text)}
+        keyboardType="default"
+        placeholder="Enter your name"
+      />
 
-      <InputFiled label="Email" placeholder="Enter your email" keyboardType="email-address" />
+      <InputFiled
+        label="Email"
+        value={email}
+        onChangeText={text => getEmail(text)}
+        placeholder="Enter your email"
+        keyboardType="email-address"
+      />
 
-      <InputFiled label="Password" placeholder="Enter your password" secureTextEntry={true} />
+      <InputFiled
+        label="Password"
+        value={password}
+        onChangeText={text => getPassword(text)}
+        placeholder="Enter your password"
+        secureTextEntry={true}
+      />
 
-      <TouchableButton value="Sign Up" onPress={() => console.warn('Enviado com sucesso!')} />
+      <TouchableButton value="Sign Up" onPress={handleCreateAccount} />
     </View>
   );
 };
