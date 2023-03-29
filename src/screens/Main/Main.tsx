a6472f9836d966045efb006488d96e716cdf9305
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-29 16:36:30 -0300

    add screen after login and authentication

---
+++ src/screens/Main/Main.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { THEME } from '../../utils/theme';
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: THEME.BLACK,
+  },
+});
+
+export default Main;
