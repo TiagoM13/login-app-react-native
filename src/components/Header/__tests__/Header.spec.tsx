b14113c906b06e709501aae3eb823bcc858037a0
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-16 14:33:32 -0300

    update header component tests

--- src/components/Header/__tests__/Header.spec.tsx
+++ src/components/Header/__tests__/Header.spec.tsx
@@ -1,27 +1,18 @@
 import Recat from 'react';
 import { fireEvent, render } from '@testing-library/react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { Header } from '../Header';
 
 describe('Header', () => {
   describe('button was rendered', () => {
     it('show the icon', () => {
-      const { getByTestId } = render(
-        <NavigationContainer>
-          <Header />
-        </NavigationContainer>,
-      );
+      const { getByTestId } = render(<Header />);
 
       const icon = getByTestId('HeaderIcon');
       expect(icon).toBeTruthy();
     });
 
     it('presses the button event', () => {
-      const { getByTestId } = render(
-        <NavigationContainer>
-          <Header />
-        </NavigationContainer>,
-      );
+      const { getByTestId } = render(<Header />);
 
       const button = getByTestId('HeaderButton');
 
