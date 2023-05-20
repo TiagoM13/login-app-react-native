06b23256eadf153e327a4656d358ab610b6821bd
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-20 15:13:57 -0300

    update regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,4 +2,5 @@
 export const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 // regex validate password
-export const REGEX_PASSWORD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#!])[0-9a-zA-Z$*&@#!]{8,}$/;
+export const REGEX_SPECIAL_CHARACTER = /^(?=.*[$*&@#!])/
+export const REGEX_UPPERCASE = /^(?=.*[A-Z])/
