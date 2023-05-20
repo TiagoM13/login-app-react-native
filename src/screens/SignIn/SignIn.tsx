47130eda9411137fd8921ec320579ebf00c044fc
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-20 13:02:27 -0300

    add hook form and validation with yup

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import { Header } from '@app/components/Header/Header';
 import { InputField } from '@app/components/InputField/InputField';
 import { TouchableButton } from '@app/components/Button/TouchableButton';
 
-import { useAuthentication } from '@app/hooks/auth';
-
 import { styles } from '@app/screens/styles/styles';
+import { REGEX_EMAIL } from '@app/utils/regex';
+
+type FormData = {
+  email: string,
+  password: string
+}
+
+const siginSchema = yup.object({
+  email: yup
+    .string()
+    .required('Informe um email')
+    .matches(REGEX_EMAIL, 'Informe um email válido'),
+  password: yup
+    .string()
+    .required('Informe um password')
+})
 
 const SignIn = () => {
-  const { email, password, setEmail, setPassword, handleAuthentication } = useAuthentication();
+  const { control, reset, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(siginSchema)
+  })
+
+  const onSubmit = (data: FormData) => {
+    console.log({ data })
+
+    reset()
+  }
 
   return (
     <View>
@@ -19,23 +44,37 @@ const SignIn = () => {
       <View style={styles.container}>
         <Text style={styles.title}>Login In</Text>
 
-        <InputField
-          label="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          placeholder="Enter your email"
-          keyboardType="email-address"
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, value } }) => (
+            <InputField
+              label="Email"
+              value={value}
+              onChangeText={onChange}
+              placeholder="Enter your email"
+              keyboardType="email-address"
+              errorMessage={errors.email?.message}
+            />
+          )}
         />
 
-        <InputField
-          label="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          placeholder="Enter your password"
-          secureTextEntry={true}
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <InputField
+              label="Password"
+              value={value}
+              onChangeText={onChange}
+              placeholder="Enter your password"
+              secureTextEntry={true}
+              errorMessage={errors.password?.message}
+            />
+          )}
         />
 
-        <TouchableButton value="Sign In" onPress={handleAuthentication} />
+        <TouchableButton value="Sign In" onPress={handleSubmit(onSubmit)} />
       </View>
     </View>
   );
