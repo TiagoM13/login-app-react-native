4797e343e5b8eebf83639ff66934ef4b1e758810
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-20 11:00:22 -0300

    create regex util

---
+++ src/utils/regex.ts
@@ -0,0 +1,5 @@
+// regex validate email
+export const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+// regex validate password
+export const REGEX_PASSWORD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#!])[0-9a-zA-Z$*&@#!]{8,}$/;
