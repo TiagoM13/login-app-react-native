4ddb3afc54a6d78e0ec17c1059d9ab4246a957aa
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-21 12:03:09 -0300

    add new functionality to screens

--- src/screens/styles/styles.ts
+++ src/screens/styles/styles.ts
@@ -1,11 +1,14 @@
 import { StyleSheet } from 'react-native';
-import { THEME } from '../../utils/theme';
+import { THEME } from '@app/utils/theme';
 
 export const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
     paddingHorizontal: 30,
-    paddingVertical: 20,
+    paddingBottom: 20,
+    flex: 1,
+  },
+  content: {
+    marginBottom: 40,
   },
   title: {
     fontFamily: 'Inter-Black',
@@ -15,4 +18,11 @@ export const styles = StyleSheet.create({
     marginTop: 20,
     color: THEME.BLACK,
   },
+  help: {
+    color: THEME.BLACK_900,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '400',
+    fontFamily: 'Inter-Light',
+  }
 });
