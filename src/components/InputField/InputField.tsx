c309a8c123667340cd425002c259534cc01280c8
TiagoM13 <thyagomotha2000@gmail.com>
2023-07-08 14:56:55 -0300

    passing all validation and data retrieval logic to the input field component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Text, TextInput, View } from 'react-native';
+import { Controller } from 'react-hook-form';
 
 import { InputFiedlProps } from '@app/entities/input';
 
 import { styles } from './styles';
 
-export const InputField = ({ label, errorMessage = null, ...rest }: InputFiedlProps) => {
+export const InputField = forwardRef<TextInput, InputFiedlProps>(({ label, errorMessage = null, inputProps, controllerProps }, ref) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>{label}</Text>
-      <TextInput {...rest} style={styles.input} placeholderTextColor={'#606060'} />
+    <Controller
+      render={({ field }) => (
+        <View style={styles.container}>
+          <Text style={styles.label}>{label}</Text>
+          <TextInput
+            ref={ref}
+            value={field.value}
+            onChangeText={field.onChange}
+            placeholderTextColor={'#606060'}
+            style={styles.input}
+            {...inputProps}
+          />
 
-      {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
-    </View>
+          {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+        </View>
+      )}
+      {...controllerProps}
+    />
   );
-};
+});
