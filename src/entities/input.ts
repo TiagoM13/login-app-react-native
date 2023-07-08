502b777ae0c5f1986f69fff2074a25fdcde76162
TiagoM13 <thyagomotha2000@gmail.com>
2023-07-08 14:54:03 -0300

    add props to input field

--- src/entities/input.ts
+++ src/entities/input.ts
@@ -1,6 +1,9 @@
 import { TextInputProps } from 'react-native';
+import { UseControllerProps } from 'react-hook-form';
 
-export interface InputFiedlProps extends TextInputProps {
+export interface InputFiedlProps {
   label: string;
   errorMessage?: string | null;
+  inputProps: TextInputProps;
+  controllerProps: UseControllerProps;
 }
