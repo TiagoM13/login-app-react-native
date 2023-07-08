b5693f7eaa0e1b291ef808cd4a3ce111a9c43a79
TiagoM13 <thyagomotha2000@gmail.com>
2023-07-08 14:54:38 -0300

    making data optional

--- src/entities/form.ts
+++ src/entities/form.ts
@@ -1,10 +1,10 @@
 export type FormDataSingUp = {
-  name: string
-  email: string
-  password: string
+  name?: string
+  email?: string
+  password?: string
 }
 
 export type FormDataSingIn = {
-  email: string
-  password: string
+  email?: string
+  password?: string
 }
